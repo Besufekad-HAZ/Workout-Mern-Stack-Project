76bf5a28492d016cafe5063f8c156ff2677cfdb4
Besufekad-HAZ <besurye@gmail.com>
2023-11-16 01:07:12 +0300

    Setup and connect database and add additional files

--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const workoutRoutes = require('./routes/workouts');
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
+// express app
 const app = express();
 
+// middleware
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
-
-app.use('/api/workouts',workoutRoutes);
-
-
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(req.path, req.method);
+  next();
 });
 
+// routes
+app.use("/api/workouts", workoutRoutes);
 
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to the database");
+      console.log(`Server is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
