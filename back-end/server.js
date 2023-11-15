524fae7957d6e16ea02cad5a702005c30a107751
Besufekad-HAZ <besurye@gmail.com>
2023-11-15 21:23:09 +0300

    Add Simple middlewares to show what requests are made on the port 4000

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,11 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
 
 app.get('/', (req, res) => {
     res.json({
