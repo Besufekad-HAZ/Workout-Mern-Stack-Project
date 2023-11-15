bd68a6638edbd417e974f42b36a4a635251551d0
Besufekad-HAZ <besurye@gmail.com>
2023-11-15 23:02:13 +0300

    Add basic routes for the workout pages

--- back-end/routes/workouts.js
+++ back-end/routes/workouts.js
@@ -9,6 +9,15 @@ router.get('/', (req, res) => {
     });
 })
 
-
+// get a single workout
+router.get('/:id', (req, res) => {
+    const workoutId = req.params.id;
+    // Logic to fetch the workout with the given id from the database
+    // ...
+    res.json({
+        workoutId: workoutId,
+        message: `Get workout with id ${workoutId}`
+    });
+});
 
 module.exports = router;
