d83c6c9f5eb30b3e7764c3484c387fdfdf8fdeb2
Besufekad-HAZ <besurye@gmail.com>
2023-11-16 16:37:46 +0300

    Create schema and basic structure and update the routes

--- back-end/routes/workouts.js
+++ back-end/routes/workouts.js
@@ -1,49 +1,53 @@
-const express = require('express');
-
+const express = require("express");
+const Workout = require("../models/Workout");
 const router = express.Router();
 
 // get all workouts
-router.get('/', (req, res) => {
-    res.json({
-        message: 'Get all workouts!!'
-    });
-})
+router.get("/", (req, res) => {
+  res.json({
+    message: "Get all workouts!!",
+  });
+});
 
 // get a single workout
-router.get('/:id', (req, res) => {
-    const workoutId = req.params.id;
-    // Logic to fetch the workout with the given id from the database
-    // ...
-    res.json({
-        workoutId: workoutId,
-        message: `Get workout with id ${workoutId}`
-    });
+router.get("/:id", (req, res) => {
+  const workoutId = req.params.id;
+  // Logic to fetch the workout with the given id from the database
+  res.json({
+    workoutId: workoutId,
+    message: `Get workout with id ${workoutId}`,
+  });
 });
 
 // create a new workout
-router.post('/', (req, res) => {
-    res.json({
-        message: '(POST) Create a new workout!!'
-    });
+router.post("/", async (req, res) => {
+  // Logic to create a new workout in the database
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // delete a workout
-router.delete('/:id', (req, res) => {
-    const workoutId = req.params.id;
-    res.json({
-        workoutId: workoutId,
-        message: `Delete a workout with id ${workoutId}`
-    });
-})
+router.delete("/:id", (req, res) => {
+  const workoutId = req.params.id;
+  res.json({
+    workoutId: workoutId,
+    message: `Delete a workout with id ${workoutId}`,
+  });
+});
 
 // update a workout
-router.patch('/:id', (req, res) => {
-   const workoutId = req.params.id;
-    res.json({
-        workoutId: workoutId,
-        message: `Update a workout with id ${workoutId}`
-    });
-})
-
+router.patch("/:id", (req, res) => {
+  const workoutId = req.params.id;
+  res.json({
+    workoutId: workoutId,
+    message: `Update a workout with id ${workoutId}`,
+  });
+});
 
 module.exports = router;
