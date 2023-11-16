f3ad4b05f3795e173bb010d0c02c98684e0bb308
Besufekad-HAZ <besurye@gmail.com>
2023-11-17 00:08:33 +0300

    Create Navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages & Components
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
