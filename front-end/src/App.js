5ca14acf545fcab5150f77483e9f598b1d79780e
Besufekad-HAZ <besurye@gmail.com>
2023-11-17 00:04:01 +0300

    Create basic sturcture routes and pages components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,19 @@
+// Date: 2021-09-05
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages & Components
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
