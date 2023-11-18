1d54974b8b5a074388b97302aea4094fa5ace22c
Besufekad-HAZ <besurye@gmail.com>
2023-11-18 22:26:00 +0300

    Update Home page layout

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,30 +1,31 @@
 import { useEffect, useState } from "react";
 
-//components
+// components
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState([]);
+  const [workouts, setWorkouts] = useState(null);
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("/api/workouts");
-      const data = await response.json();
+      const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(data);
+        setWorkouts(json);
       }
     };
+
     fetchWorkouts();
   }, []);
 
   return (
-    <div className="Home">
+    <div className="home">
       <div className="workouts">
         {workouts &&
           workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout} />
+            <WorkoutDetails workout={workout} key={workout._id} />
           ))}
       </div>
       <WorkoutForm />
