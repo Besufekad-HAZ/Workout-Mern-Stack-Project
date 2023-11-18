b52105f40ed7ae25b137484de2ce652b8904d072
Besufekad-HAZ <besurye@gmail.com>
2023-11-18 23:19:19 +0300

    Import Dispatch from useWorkoutsContext hook

--- front-end/src/components/WorkoutDetails.js
+++ front-end/src/components/WorkoutDetails.js
@@ -1,11 +1,14 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
   const handleClick = async () => {
     const response = await fetch(`/api/workouts/${workout._id}`, {
       method: "DELETE",
     });
     const json = await response.json();
 
-    if(response.ok) {
+    if (response.ok) {
       dispatch({ type: "DELETE_WORKOUT", payload: json });
     }
   };
