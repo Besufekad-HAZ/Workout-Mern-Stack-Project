adc19a729e755cb39f285d1ec172ca80b23c00ce
Besufekad-HAZ <besurye@gmail.com>
2023-11-18 21:46:59 +0300

    Add forms and implement fetch

---
+++ front-end/src/components/WorkoutForm.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const workout = { title, load, reps };
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Workout</h3>
+
+      <label htmlFor="">Exercise Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <label htmlFor="">Reps</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+      <label htmlFor="">Load (In kg):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+      <button>Submit</button>
+    </form>
+  );
+};
