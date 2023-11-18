24af832ccf4fec196b548e6dadf42395ddfb95b4
Besufekad-HAZ <besurye@gmail.com>
2023-11-18 23:17:57 +0300

    Define and Used Workout context providers

--- front-end/src/components/WorkoutDetails.js
+++ front-end/src/components/WorkoutDetails.js
@@ -1,4 +1,15 @@
 const WorkoutDetails = ({ workout }) => {
+  const handleClick = async () => {
+    const response = await fetch(`/api/workouts/${workout._id}`, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if(response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -11,6 +22,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
