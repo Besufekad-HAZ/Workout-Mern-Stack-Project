c83ba75bd9a5fb97566e3a0b8b8772ee6fa44b5e
Besufekad-HAZ <besurye@gmail.com>
2023-11-27 21:59:08 +0300

    Implement Auth context with provider

--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -17,4 +17,12 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  console.log("AuthContext state: ", state);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
