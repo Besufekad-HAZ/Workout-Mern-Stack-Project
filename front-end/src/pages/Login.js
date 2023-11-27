1a87b4d58b4a2e4f4346c9a6d9371e1a28c1e802
Besufekad-HAZ <besurye@gmail.com>
2023-11-27 22:37:06 +0300

    Add Login in form component too

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Login</h3>
+
+      <label>Email:</label>
+      <input
+        type="email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
