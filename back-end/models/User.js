5f89e40c73e86bde12cd028c6f51f34fc4678243
Besufekad-HAZ <besurye@gmail.com>
2023-11-27 15:27:16 +0300

    Use bcrypt to hash user passwords

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -8,12 +9,19 @@ const userSchema = new Schema({
 });
 
 //static signup method
-userSchema.static.signup = async function (email, password) {
+userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email });
 
   if (exists) {
     throw Error("Email already in use");
   }
+  // use bcrypt to hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
 };
 
 module.exports = mongoose.model("User", userSchema);
