5c3a54b808df178b26ff056244b526e8d1ff5c18
Besufekad-HAZ <besurye@gmail.com>
2023-11-27 19:59:27 +0300

    Create Static Login Method

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -36,4 +36,30 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Email not found");
+  }
+
+  // compare password
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw Error("Password is incorrect");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
