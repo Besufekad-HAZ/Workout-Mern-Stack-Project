41990784e26c8b1590e207e06ae489063c68b2b9
Besufekad-HAZ <besurye@gmail.com>
2023-11-27 20:12:47 +0300

    Implement User Login In Users Controller

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -8,7 +8,18 @@ const createToken = (_id) => {
 
 // Login Controller
 const loginUser = async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 // Signup Controller
 const signupUser = async (req, res) => {
