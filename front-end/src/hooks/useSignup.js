c1a49a7010f0a332c6c8cf6467aa4353c78eace0
Besufekad-HAZ <besurye@gmail.com>
2023-11-28 21:21:14 +0300

    Update use signup custom hooks

--- front-end/src/hooks/useSignup.js
+++ front-end/src/hooks/useSignup.js
@@ -9,23 +9,28 @@ export const useSignup = () => {
   const signup = async (email, password) => {
     setIsLoading(true);
     setError(null);
-    try {
-      const response = await fetch("/api/users/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await response.json();
-      if (data.error) {
-        throw new Error(data.message);
-      }
-      dispatch({ type: "SIGNUP", payload: data });
-    } catch (error) {
-      setError(error.message);
-    } finally {
+
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await response.json();
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      // save the user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+
       setIsLoading(false);
     }
   };
+  return { signup, error, isLoading };
 };
