edfa302acf267957f287e7ee89d17433a45c313f
Besufekad-HAZ <besurye@gmail.com>
2023-11-28 21:02:18 +0300

    Setup and use custom hook named useSignup

---
+++ front-end/src/hooks/useSignup.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const signup = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("http://localhost:3001/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (data.error) {
+        throw new Error(data.message);
+      }
+      dispatch({ type: "SIGNUP", payload: data });
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+};
