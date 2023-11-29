c6dc680eb11d40e1089848a47c975d44e7298aef
Besufekad-HAZ <besurye@gmail.com>
2023-11-29 14:18:05 +0300

    Update the Workout form based on useAuth contexnt

--- front-end/src/components/WorkoutForm.js
+++ front-end/src/components/WorkoutForm.js
@@ -4,6 +4,7 @@ import { useAuthContext } from "../hooks/useAuthContext";
 
 const WorkoutForm = () => {
   const { dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
@@ -14,6 +15,11 @@ const WorkoutForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!user) {
+      setError("You must be logged in");
+      return;
+    }
+
     const workout = { title, load, reps };
 
     const response = await fetch("/api/workouts", {
@@ -21,6 +27,7 @@ const WorkoutForm = () => {
       body: JSON.stringify(workout),
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
       },
     });
     const json = await response.json();
