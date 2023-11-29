5bb5ecec303f86957e6ca261c3454345e0ef08f6
Besufekad-HAZ <besurye@gmail.com>
2023-11-29 19:32:28 +0300

    Fix a user to view only his/her workout based on the id provided instead of all in the getAllWorkouts method

--- back-end/controllers/workoutController.js
+++ back-end/controllers/workoutController.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 // get all workouts
 const getAllWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  const user_id = req.user._id;
+
+  const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 });
 
   res.status(200).json(workouts);
 };
