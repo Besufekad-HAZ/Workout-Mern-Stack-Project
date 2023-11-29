796d3087506f786c907f30969402891d06322325
Besufekad-HAZ <besurye@gmail.com>
2023-11-29 13:40:43 +0300

    Implement authorization token check in requireAuth

--- back-end/middleware/requireAuth.js
+++ back-end/middleware/requireAuth.js
@@ -1,3 +1,7 @@
 const requireAuth = (req, res, next) => {
-    
-}
+  //verify user is authenticated
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+};
